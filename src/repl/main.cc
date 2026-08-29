b9cea9110fc2ac8d8cd8adc963226e7944461aeb
Bruno Marques <brunosmarques1995@hotmail.com>
2026-08-28 22:53:43 -0300

    Added windows cli support for utf-8

--- src/repl/main.cc
+++ src/repl/main.cc
@@ -2,8 +2,19 @@
 #include <cstdlib>
 #include "Repl.hh"
 
+#ifdef STAR_PLATFORM_WINDOWS
+
+#include <windows.h>
+
+#endif // 
+
+
 int main(int argc, char** argv)
 {
+#ifdef STAR_PLATFORM_WINDOWS
+    SetConsoleCP(CP_UTF8);
+    SetConsoleOutputCP(CP_UTF8);
+#endif
     if(argc > 2)
     {
         std::cerr << "Usage: " << argv[0] << " file.st" << std::endl;
